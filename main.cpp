e819090b6694f54f0cf8f7927cb045bc46969229
patrickgeorge1 <patrionpatrick@gmail.com>
2021-01-09 16:08:00 +0200

    main example

--- main.cpp
+++ main.cpp
@@ -9,18 +9,27 @@ using namespace std;
 
 int main(int argc, char** argv) {
     TextProcessor tp;
-    ParagraphPiece * pp = new ParagraphPiece(1, 1, 3, "Salut sunt Andy, toata lumea ma cunoaste bai bro !\n Si ce daca rad, si ce daca plang a ?");
+    ParagraphPiece * p1 = new ParagraphPiece(1, 1, 3, "Salut sunt Andy, toata lumea ma cunoaste bai bro !\n Si ce daca rad, si ce daca plang a ?");
+    ParagraphPiece * p2 = new ParagraphPiece(1, 1, 2, "Hello sanky, montana mistere");
+    ParagraphPiece * p3 = new ParagraphPiece(1, 1, 1, "Au innebunit salcamii !");
     ConcurrentMemory * cm = new ConcurrentMemory(3);
-    cm->pushPieceToThread(1, pp);
-    cm->pushPieceToThread(1, pp);
-    cm->pushPieceToThread(2, pp);
+    cm->pushPieceToThread(1, p1);
+    cm->pushPieceToThread(1, p2);
+    cm->pushPieceToThread(2, p3);
 
     cout << cm->getNumberOfPiecesFromThread(0) << endl;
     cout << cm->getNumberOfPiecesFromThread(1) << endl;
     cout << cm->getNumberOfPiecesFromThread(2) << endl;
+    cout << endl;
 
-    cout << cm->getNextPieceForThread(1)->getLines();
-    
+    cout << cm->getNextPieceForThread(1)->getLines() << endl; cm->removePieceFromThread(1);
+    cout << cm->getNextPieceForThread(1)->getLines() << endl;
+    cout << cm->getNextPieceForThread(2)->getLines() << endl;
+    cout << endl;
+
+    ParagraphPiece * test = cm->getNextPieceForThread(2);
+    tp.processPiece(*test);
+    cout << test->getLines() << endl;
 
     return 0;
 }
